d890e893a9f31d1683c7716e6a6184cbc85093a0
deswelle-j <deswelle.johan@gmail.com>
2019-10-02 21:18:49 +0200

    Add twig on project and migrate php's view file in twig file #11

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 
 require ('../vendor/autoload.php');
+$loader = new \Twig\Loader\FilesystemLoader('../src/blog/view/frontend');
+$twig = new \Twig\Environment($loader, [
+    'debug' => true
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
 
 require('../src/blog/controller/frontend.php');
 
@@ -12,27 +17,24 @@ try
 
         if($_GET['action'] == 'listPosts')
         {
-            listPosts();
+            listPosts($twig);
         }
 
         if($_GET['action'] == 'post')
         {
             if(isset($_GET['id']) && $_GET['id'] >= 0)
             {
-                post();
+                post($twig);
             }
             else
             {
                 throw new Exception('Aucun identifiant de billet envoyé');
             }
         }
-
         if($_GET['action'] == 'addComment')
         {
-
             if(isset($_GET['id']) && $_GET['id'] >= 0)
             {
-
                 if (!empty($_POST['author']) && !empty($_POST['comment']))
                 {
                     addComment($_GET['id'], $_POST['author'], $_POST['comment']);
@@ -41,14 +43,12 @@ try
                 {
                     throw new Exception('Tous les champs ne sont pas remplis !');
                 }
-
             }
             else
             {
                 throw new Exception('Aucun identifiant de billet envoyé');
             }
         }
-
         if($_GET['action'] == 'edit')
         {
             if(isset($_GET['id']) && $_GET['id'] >=0 && isset($_GET['post_id']) && $_GET['post_id'] >=0 )
@@ -57,23 +57,19 @@ try
             }
             else
             {
-
                 throw new Exception('Erreur : identifiant de commentaire ou identifiant de billet non envoyé');
             }
         }
     }
-
     else
     {
-        listPosts();
+        listPosts($twig);
     }
-
 }
 catch(Exception $e)
 {
     $errorMessage = $e->getMessage();
-    
-    require('view/errorView.php');
+    require('../src/blog/view/frontend/errorView.php');
 }
 
 
--- src/blog/controller/frontend.php
+++ src/blog/controller/frontend.php
@@ -1,41 +1,29 @@
 <?php
-// require_once('model/PostManager.php');
-
-// require_once('model/CommentManager.php');
-
 use Framework\Blog\Model\PostManager;
-
 use Framework\Blog\Model\CommentManager;
 
-
-function listPosts()
+function listPosts($twig)
 {
     $postManager = new PostManager();
-
-    $posts = $postManager->getPosts();
-
-    require('../src/blog/view/frontend/listPostsView.php');
+    $posts = $postManager->getPosts($twig);
+    echo $twig->render('listPosts.html.twig', ['posts' => $posts ]);
 }
-
-function post()
+function post($twig)
 {
     $postManager = new PostManager();
-
     $commentManager = new CommentManager();
-
     $post = $postManager->getPost($_GET['id']);
-
     $comments = $commentManager->getComments($_GET['id']);
-
-    require('../src/blog/view/frontend/postView.php');
+    echo $twig->render('postView.html.twig', [
+        'post' => $post,
+        'comments' => $comments
+         ]);
 }
 
 function addComment($postId, $author, $comment)
 {
     $commentManager = new CommentManager();
-
     $affectedLines = $commentManager->postComment($postId, $author, $comment);
-
     if ($affectedLines === false) {
         throw new Exception('Impossible d\'ajouter le commentaire !');
     }
@@ -43,21 +31,15 @@ function addComment($postId, $author, $comment)
         header('Location: index.php?action=post&id=' . $postId);
     }
 }
-
-function edit()
+function edit($twig)
 {
     // 2 options : no data from the form or the data is present
-
     if(isset($_POST['modif']))
     {
-        
         $commentManager = new CommentManager();
-
         $edit = $commentManager->editComment($_GET['id'],$_POST['modif']);
-
         header("Location:index.php?action=post&id=".$_GET['post_id']);
     }
-
     else
     {
         require('../src/blog/view/frontend/editView.php');
