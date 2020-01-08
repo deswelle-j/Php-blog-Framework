3f38ae8678823f7be7d99d4eaa4de09521a53c5b
deswelle-j <deswelle.johan@gmail.com>
2020-01-08 21:28:22 +0100

    add controle on user role to display list post

--- src/blog/model/PostManager.php
+++ src/blog/model/PostManager.php
@@ -15,6 +15,7 @@ class PostManager extends Manager
             DATE_FORMAT(modification_date, \'%d/%m/%Y à %Hh%imin%ss\') AS modification_date_fr 
             FROM posts
             LEFT JOIN users ON posts.author = users.id
+            WHERE published = 1
             ORDER BY creation_date  
             DESC 
             LIMIT 0, 4'
@@ -23,7 +24,6 @@ class PostManager extends Manager
         $postsReq = $req->fetchAll();
         $postAll = [];
         foreach ($postsReq as $reqPost) {
-            var_dump($reqPost);
             $post  = new Post(
                 $reqPost['id'],
                 $reqPost['title'],
@@ -45,15 +45,65 @@ class PostManager extends Manager
         $db = $this->dbConnect();
         $req = $db->query(
             'SELECT posts.id, title, content, kicker, username, published, 
-            DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr
+            DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr,
             DATE_FORMAT(modification_date, \'%d/%m/%Y à %Hh%imin%ss\') AS modification_date_fr 
             FROM posts 
             LEFT JOIN users ON posts.author = users.id
+            WHERE published = 1
             ORDER BY creation_date 
             DESC 
             LIMIT 0, 5'
         );
         $req->execute();
+        $postsReq = $req->fetchAll();
+        $postAll = [];
+        foreach ($postsReq as $reqPost) {
+            $post  = new Post(
+                $reqPost['id'],
+                $reqPost['title'],
+                $reqPost['kicker'],
+                $reqPost['username'],
+                $reqPost['content'],
+                $reqPost['creation_date_fr'],
+                $reqPost['modification_date_fr'],
+                $reqPost['published']
+            );
+            array_push($postAll, $post);
+        }
+        $req->closeCursor();
+        return $postAll;
+    }
+
+    public function getAdminPosts( $username = false)
+    {
+        $db = $this->dbConnect();
+        if ($username !== false) {
+            $req = $db->query(
+                'SELECT posts.id, title, content, kicker, username, published, 
+                DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr,
+                DATE_FORMAT(modification_date, \'%d/%m/%Y à %Hh%imin%ss\') AS modification_date_fr 
+                FROM posts 
+                LEFT JOIN users ON posts.author = users.id
+                WHERE published = 1
+                ORDER BY creation_date 
+                DESC 
+                LIMIT 0, 5'
+            ); 
+            $req->execute();
+        } else {
+            $req = $db->query(
+                'SELECT posts.id, title, content, kicker, username, published, 
+                DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr,
+                DATE_FORMAT(modification_date, \'%d/%m/%Y à %Hh%imin%ss\') AS modification_date_fr 
+                FROM posts 
+                LEFT JOIN users ON posts.author = users.id
+                WHERE published = 1 AND username = 
+                ORDER BY creation_date 
+                DESC 
+                LIMIT 0, 5'
+            );
+            $req->execute(array($username));
+        }
         $postsReq = $req->fetchAll();
         $postAll = [];
         foreach ($postsReq as $reqPost) {
